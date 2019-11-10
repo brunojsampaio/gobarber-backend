91eae630b3ea97f37febf31cb7142357281d0d71
Bruno Sampaio <brunojsampaio@gmail.com>
2019-11-10 11:17:45 -0300

    Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'f64272b31fc46c',
+    pass: '9cd522e57784d8',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
