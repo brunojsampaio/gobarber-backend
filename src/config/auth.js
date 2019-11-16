9ac65573811708c5dcedec5f4c617b920081623f
Bruno Sampaio <brunojsampaio@gmail.com>
2019-11-16 12:53:15 -0300

    Variáveis ambiente

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '3433bead2926b256e4bbf23001dd7c2b',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://b87de70d1c4b46f986436404d3a9a09a@sentry.io/1823418',
+  dsn: process.env.SENTRY_DSN,
 };
